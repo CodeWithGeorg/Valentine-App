f6d325daa7f4e433c322bfd0f300af6de7ef966b
CodeWithGeorg <gkiarie816@gmail.com>
2026-02-12 23:45:37 +0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,11 +29,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${inter.className} min-h-screen bg-gradient-to-br from-pink-100 via-red-50 to-pink-200`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
